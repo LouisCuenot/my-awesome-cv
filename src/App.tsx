cb03b217e02a62bf7a363d75cc3f3b44483803ba
Louis Cuenot <louis.cuenot@nfinite.app>
2022-12-20 20:30:31 +0100

    MyAwesomeCV

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [test, setTest] = useState<string>('Test')  
+
+  useEffect(()=>{
+    if (
+        (
+          navigator.userAgent.indexOf('iPhone') != -1 || 
+          navigator.userAgent.indexOf('iPad') != -1 || 
+          navigator.userAgent.indexOf('Android') != -1 || 
+          navigator.userAgent.indexOf('Windows Phone') != -1
+        ) && window.DeviceOrientationEvent
+    ) {
+      setTest('Mobile')
+    } else {
+      setTest('Ordi')
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{test}</span>
     </div>
   );
 }
